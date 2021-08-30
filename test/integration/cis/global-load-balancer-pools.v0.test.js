48e0348c984159d88ac27e61fc425e55f6e91664
ajay-malhotra1 <ajay.malhotra1@ibm.com>
2021-08-30 16:36:29 -0700

    chore: disable few tests because of env issue

--- test/integration/cis/global-load-balancer-pools.v0.test.js
+++ test/integration/cis/global-load-balancer-pools.v0.test.js
@@ -33,7 +33,7 @@ const describe = authHelper.prepareTests(configFile);
 // config properties, rather than let the SDK do it for us.
 const config = authHelper.loadConfig();
 
-describe('GLBPoolApi', () => {
+describe.skip('GLBPoolApi', () => {
   jest.setTimeout(timeout);
 
   // Initialize the service client.
